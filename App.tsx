dab21e8a57fa13420616cb3a6e773b7e4c415b2f
Helena Paixao <helena.paixao@corteva.com>
2022-10-09 23:37:10 -0300

    fix: config splashscreen in app

--- App.tsx
+++ App.tsx
@@ -1,25 +1,32 @@
 
-import { useFonts } from 'expo-font'
 import * as SplashScreen from "expo-splash-screen"
-import {Inter_400Regular, Inter_700Bold} from '@expo-google-fonts/inter'
+import {useFonts,Inter_400Regular, Inter_700Bold} from '@expo-google-fonts/inter'
 import Home from './src/pages/Home'
-import AppLoading from 'expo-app-loading'
+import { useCallback } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_700Bold
-  })
+  });
 
-  if(!fontsLoaded) {
-    return <AppLoading/>
-  } else {
-    return (
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if(!fontsLoaded) return null;
+
+  return (
+    <SafeAreaView onLayout={onLayoutRootView}>
       <Home/>
-    )
-  }
+    </SafeAreaView>
+  )
  
 }
 
